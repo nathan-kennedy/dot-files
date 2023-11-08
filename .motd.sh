6caec3c73def8325c55668729e29b609a7403028
Nathan <6779051+nathan-kennedy@users.noreply.github.com>
2023-11-07 20:52:38 -0500

    save

--- .motd.sh
+++ .motd.sh
@@ -1,31 +1,3 @@
-cols=$(tput cols)
-block_size=5
-pattern=(40 45 41 43 42 44 46 47)  # Black, Magenta, Red, Yellow, Green, Blue, Teal, White
-pattern_length=${#pattern[@]}
-blocks_needed=$((cols / block_size))
-printed_blocks=0
-chars_printed=0
-while [ $printed_blocks -lt $blocks_needed ]; do
-  for color in "${pattern[@]}"; do
-    if [ $printed_blocks -ge $blocks_needed ]; then
-      break
-    fi
-    if [ $((chars_printed + block_size)) -gt $cols ]; then
-      # Calculate remaining spaces and print a smaller block
-      remaining=$((cols - chars_printed))
-      printf "\033[%sm%${remaining}s\033[0m" "$color" " "
-      chars_printed=$((chars_printed + remaining))
-    else
-      printf "\033[%sm%${block_size}s\033[0m" "$color" " "
-      chars_printed=$((chars_printed + block_size))
-    fi
-    printed_blocks=$((printed_blocks + 1))
-  done
-done
-# Move to the next line
-printf "\n"
-echo "                                                                       "
-echo "                                                                       "
 echo "               \033[1;0m.:^~~~~~^:                                     "           
 echo "           \033[1;0m^7YG#&@@@@@@@&B57:                                 "           
 echo "        \033[1;0m^JB@@@@@@@@@@@@@@@@@&G7.                              "           
@@ -44,7 +16,19 @@ echo "   \033[1;0m.5@Y    :@@P555B@@@P     ""\033[1;36mNetwork:\033[34m $(networ
 echo "     \033[1;0m5@G?J!J@B!:  ^G5J^     ""\033[1;36mDiskspace:\033[34m $(df -h / | awk 'NR==2 {print $4 " available"}' | sed 's/Gi/GB/')\033[0m" 
 echo "     \033[1;0m~PB#&@BJ~^:            ""\033[1;36mUptime:\033[34m $(uptime | awk -F'up ' '{print $2}' | cut -d',' -f1-2)\033[0m"
 echo "       \033[1;0m...7.                ""\033[1;36mPackages:\033[34m $(brew list | wc -l | tr -d ' ')\033[0m"
-echo "                                 "
-echo "                                 "
-echo "                                 "
-echo -e "\033[0m\033[1;37mWelcome, Nathan! Today is\033[1;32m $(date +"%a %b %e")\033[1;37m\033[0m Happy coding!\033[0m"
+
+# Define color variables with background colors
+BG_BLACK="\033[0;40m"
+BG_RED="\033[0;41m"
+BG_GREEN="\033[0;42m"
+BG_YELLOW="\033[0;43m"
+BG_BLUE="\033[0;44m"
+BG_MAGENTA="\033[0;45m"
+BG_CYAN="\033[0;46m"
+BG_WHITE="\033[0;47m"
+RESET="\033[0m"
+
+# Print a line with the 8 main terminal background colors as blocks
+echo -e "         T                  ""${BG_BLACK}     ${RESET}${BG_RED}     ${RESET}${BG_GREEN}     ${RESET}${BG_YELLOW}     ${RESET}${BG_BLUE}     ${RESET}${BG_MAGENTA}     ${RESET}${BG_CYAN}     ${RESET}${BG_WHITE}     ${RESET}"
+echo -e "   "
+echo -e "\033[0m\033[1;37mWelcome, $USER! Today is\033[1;32m $(date +"%a %b %e")\033[1;37m\033[0m Happy coding!\033[0m"
