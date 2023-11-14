547b760361ccf814904620aa60115906092cb36c
Nathan <6779051+nathan-kennedy@users.noreply.github.com>
2023-11-13 20:23:28 -0500

    save 11.13.23

--- .alias.sh
+++ .alias.sh
@@ -14,6 +14,7 @@ alias ga='git add'
 alias gs='git status'
 alias gc='git commit -m'
 alias gp='git push origin'
+alias gd='git diff'
 alias rm='rm -v -I'
 alias c="clear && neofetch" 
 alias mkcd='foo(){ mkdir -p "$1"; cd "$1" }; foo '
@@ -24,10 +25,7 @@ alias rms='rm -i -v'
 alias la='ls -A'
 alias ls='ls -G'
 alias l='ls -lah'
-alias ll='ls -lh'
-alias brave='open -a "Brave Browser.app"'
-alias logseq='open -a Logseq'
-alias chathistory='code /Users/nate/Library/Application\ Support/aichat/messages.md'
+alias chathistory='code $HOME/Library/Application\ Support/aichat/messages.md'
 #check network speed/quality
 alias speedtest='speedtest-cli'
 #copy to clipboard (paste board)
@@ -40,22 +38,19 @@ alias repos="cd ~/git_repos"
 alias trash='trash -F'
 #Flush DNS cache - password needed
 alias dnsflush='sudo killall -HUP mDNSResponder && sudo killall mDNSResponderHelper && sudo dscacheutil -flushcache'
-#alias r='clear && source ~/.zshrc' 
 alias r='clear && source ~/.zshrc'
-
-#full 'ps aux' output sorted by memory (less cmd args, pid, and vsz) with dashed line row separators
-alias psmem="ps aux | sort -nr -k 4 | head -11 | awk 'BEGIN { count=1; } NR==1 {print \"NO   USER               %CPU  %MEM    USAGE   COMMAND\"; print \"---- ------------------- ----- ------ ------- --------------------------------\"} NR > 1 {printf \"%-4d %-20s %-6s %-7s %-8s %s\n\", count++, \$1, \$3, \$4, \$6/1024, \$11; print \"---- ------------------- ----- ------ ------- --------------------------------\"}' | column -t"
-#full 'ps aux' output sorted by cpu (less cmd args, pid, and vsz) with dashed line row separators
-alias pscpu="ps aux | sort -nr -k 3 | head -11 | awk 'BEGIN { count=1; } NR==1 {print \"NO   USER               %CPU  %MEM    USAGE   COMMAND\"; print \"---- ------------------- ----- ------ ------- --------------------------------\"} NR > 1 {printf \"%-4d %-20s %-6s %-7s %-8s %s\n\", count++, \$1, \$3, \$4, \$6/1024, \$11; print \"---- ------------------- ----- ------ ------- --------------------------------\"}' | column -t"
-
-alias dusage="du -hd 1 | sort -hr | awk 'BEGIN { print \"SIZE     DIRECTORY\"; print \"------------------\" } { print \$1, \$2 }'"
-
 alias greph='history | grep $1'
 alias grepa='alias | grep $1'
 alias grep='grep --color=auto'
-#alias fgrep='fgrep --color=auto'
-#alias egrep='egrep --color=auto'
 alias egrep='grep -E --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
 alias fgrep='grep -F --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
 #open zshrc config and MOTD in VS Code
 alias zsh='code ~/.zshrc ~/.zsh/.alias.sh ~/.zsh/.motd.sh ~/.zsh/.functions.sh'
+alias dusage="du -hd 1 | sort -hr | awk 'BEGIN { print \"SIZE     DIRECTORY\"; print \"------------------\" } { print \$1, \$2 }'"
+#Apps
+alias brave='open -a "Brave Browser.app"'
+alias logseq='open -a Logseq'
+#full 'ps aux' output sorted by memory (less cmd args, pid, and vsz) with dashed line row separators
+#alias psmem="ps aux | sort -nr -k 4 | head -11 | awk 'BEGIN { count=1; } NR==1 {print \"NO   USER               %CPU  %MEM    USAGE   COMMAND\"; print \"---- ------------------- ----- ------ ------- --------------------------------\"} NR > 1 {printf \"%-4d %-20s %-6s %-7s %-8s %s\n\", count++, \$1, \$3, \$4, \$6/1024, \$11; print \"---- ------------------- ----- ------ ------- --------------------------------\"}' | column -t"
+#full 'ps aux' output sorted by cpu (less cmd args, pid, and vsz) with dashed line row separators
+#alias pscpu="ps aux | sort -nr -k 3 | head -11 | awk 'BEGIN { count=1; } NR==1 {print \"NO   USER               %CPU  %MEM    USAGE   COMMAND\"; print \"---- ------------------- ----- ------ ------- --------------------------------\"} NR > 1 {printf \"%-4d %-20s %-6s %-7s %-8s %s\n\", count++, \$1, \$3, \$4, \$6/1024, \$11; print \"---- ------------------- ----- ------ ------- --------------------------------\"}' | column -t"
