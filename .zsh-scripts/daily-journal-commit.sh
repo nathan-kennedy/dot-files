822b9344c43c92c8513531c86fa247b371f1f1f6
Nathan <6779051+nathan-kennedy@users.noreply.github.com>
2023-12-11 09:07:39 -0500

    save 12.11.23

---
+++ .zsh-scripts/daily-journal-commit.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+# Ensure .zshrc is loaded
+source $HOME/.zshrc
+
+# Get the current date
+date=$(date +"%Y_%m_%d")
+
+# Commit message
+commit_message="save $(date +"%m.%d.%y")"
+
+# Logseq journal directory
+journal_dir="/Users/nate/Documents/Logseq-git/LogSeq-PKB/journals"
+
+# Today's journal file path
+journal_file="${journal_dir}/${date}.md"
+
+# Check if today's journal file exists
+if [ ! -f "$journal_file" ]; then
+	# The file does not exist, create it
+	touch "$journal_file"
+fi
+
+# Run your acp function with the formatted date
+acp $commit_message
+
+# temporary log file to confirm launchd tasking is working as intended
+echo "$(date): Daily Journal Script Ran via launchd." >> /Users/nate/Documents/Logseq-git/LogSeq-PKB/logs
--- .zshrc
+++ .zshrc
@@ -1,5 +1,7 @@
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/local/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/bin:/var/run/com.apple.security.cryptexd/codex.system/bootstrap/usr/appleinternal/bin"
 
+export EDITOR=nvim
+
 # Loads version control information for use in the prompt
 autoload -Uz vcs_info
 precmd() { vcs_info }
